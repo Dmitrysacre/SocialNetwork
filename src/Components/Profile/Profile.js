a85c0d5ec94ab5ca517cf101590ceefaaa3e4409
Dmitry Nichiporenko <dmitrynichi@gmail.com>
2020-12-26 13:39:19 +0300

    posts reated

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Avatar from './Avatar'
+import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
     return (
             <div className="col">
                 <h1>Profile</h1>
                 <Avatar></Avatar>
+                <MyPosts></MyPosts>
             </div>
     )
 }
