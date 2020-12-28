cad1c24e844716cfca7349481262cf664ad9b7de
Dmitry Nichiporenko <dmitrynichi@gmail.com>
2020-12-28 11:37:44 +0300

    routing created

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import Header from './Components/Header/Header'
 import Navbar from './Components/Navbar/Navbar'
 import Profile from './Components/Profile/Profile'
 import Dialogs from './Components/Dialogs/Dialogs'
+import News from './Components/News/News'
+import Music from './Components/Music/Music'
+import Settings from './Components/Settings/Settings'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
 function App() {
 
-
   return (
+   <BrowserRouter>
     <div className="container">
       <Header></Header>
 
       <div className="row pt-3">
       <Navbar></Navbar>
-      <Profile></Profile>
+      <Switch>
+      <Route path='/dialogs' component={Dialogs}></Route>
+      <Route path='/profile' component={Profile}></Route>
+      <Route path='/news' component={News}></Route>
+      <Route path='/music' component={Music}></Route>
+      <Route path='/settings' component={Settings}></Route>
+      </Switch>
       </div>
 
     </div>
+   </BrowserRouter>
   )
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
       <div className="col-3">
-        <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-          <a className="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-          <a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-          <a className="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-          <a className="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">News</a>
-          <a className="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Music</a>
-          <a className="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+        <div className="nav flex-column nav-pills">
+          <NavLink className="nav-link" to="/profile">Profile</NavLink>
+          <NavLink className="nav-link" to="/dialogs">Messages</NavLink>
+          <NavLink className="nav-link" to="/news">News</NavLink>
+          <NavLink className="nav-link" to="/music">Music</NavLink>
+          <NavLink className="nav-link" to="/settings">Settings</NavLink>
         </div>
       </div>
     )
